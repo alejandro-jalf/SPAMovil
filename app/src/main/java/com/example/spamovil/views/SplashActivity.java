8ee3b4f7893b055e71a47fb491d19ef66f3de70e
alejandro-jalf <aleflo_1996@outlook.com>
2022-03-17 12:28:22 -0600

    ADD: Agrega Splash para aplicacion

---
+++ app/src/main/java/com/example/spamovil/views/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.spamovil.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.spamovil.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
