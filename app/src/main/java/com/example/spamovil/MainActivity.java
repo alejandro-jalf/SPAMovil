d37ee0f57be88a2df8094fbcb85f1e80787df658
alejandro-jalf <aleflo_1996@outlook.com>
2022-03-24 13:23:06 -0600

    FIX: Corrige error de navegacion drawer layout

--- app/src/main/java/com/example/spamovil/MainActivity.java
+++ app/src/main/java/com/example/spamovil/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.spamovil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -10,9 +11,17 @@ import com.example.spamovil.Services.Instances;
 import com.example.spamovil.controllers.ControllerConfigs;
 import com.example.spamovil.models.Configs;
 import com.example.spamovil.models.Users;
+import com.example.spamovil.ui.ChecadorPreciosFragment;
+import com.example.spamovil.ui.home.HomeFragment;
+import com.example.spamovil.ui.slideshow.SlideshowFragment;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -24,11 +33,14 @@ import com.example.spamovil.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Instances instances;
     private ControllerConfigs controllerConfigs;
     private Configs configs;
     private AppBarConfiguration mAppBarConfiguration;
+    private ActionBar actionBar;
     private ActivityMainBinding binding;
     private int visita = 0;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,44 +50,86 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_26);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
+        instances = new Instances();
         controllerConfigs = Instances.getControllerConfigs();
 
-        DrawerLayout drawer = binding.drawerLayout;
+        drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
+        /*mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_checador_precios, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+                .build();*/
+
+        setupNavigationDrawerContent(navigationView);
+        /* NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-        //setTabMain(navController);
+        NavigationUI.setupWithNavController(navigationView, navController); */
+        setTabMain();
     }
 
-    private void setTabMain(NavController navController) {
-        configs = controllerConfigs.getConfig("TabMain");
-        switch (configs.getValue()) {
-            case "Inicio":
-                navController.navigate(R.id.nav_home);
-                break;
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        item.setChecked(true);
+                        setFragment("Inicio");
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.nav_checador_precios:
+                        item.setChecked(true);
+                        setFragment("Checador de Precios");
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                    case R.id.nav_slideshow:
+                        item.setChecked(true);
+                        setFragment("Codificador de articulos");
+                        drawer.closeDrawer(GravityCompat.START);
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setFragment(String tab) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (tab) {
             case "Checador de Precios":
-                navController.navigate(R.id.nav_checador_precios);
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                ChecadorPreciosFragment checadorPreciosFragment = new ChecadorPreciosFragment();
+                fragmentTransaction.replace(R.id.nav_host_fragment_content_main, checadorPreciosFragment);
+                fragmentTransaction.commit();
                 break;
             case "Codificador de articulos":
-                navController.navigate(R.id.nav_slideshow);
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                SlideshowFragment slideshowFragment = new SlideshowFragment();
+                fragmentTransaction.replace(R.id.nav_host_fragment_content_main, slideshowFragment);
+                fragmentTransaction.commit();
+                break;
+            default:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                HomeFragment homeFragment = new HomeFragment();
+                fragmentTransaction.replace(R.id.nav_host_fragment_content_main, homeFragment);
+                fragmentTransaction.commit();
                 break;
         }
-        visita++;
+    }
+
+    private void setTabMain() {
+        configs = controllerConfigs.getConfig("TabMain");
+        setFragment(configs.getValue());
     }
 
     @Override
@@ -86,9 +140,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    } */
+
+    /* public static void fullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+    } */
 }
--- app/src/main/java/com/example/spamovil/ui/ChecadorPreciosFragment.java
+++ app/src/main/java/com/example/spamovil/ui/ChecadorPreciosFragment.java
@@ -9,41 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.spamovil.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChecadorPreciosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.zip.Inflater;
+
 public class ChecadorPreciosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FloatingActionButton fabFullScreen;
 
     public ChecadorPreciosFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChecadorPreciosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ChecadorPreciosFragment newInstance(String param1, String param2) {
         ChecadorPreciosFragment fragment = new ChecadorPreciosFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +31,25 @@ public class ChecadorPreciosFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        if (getArguments() != null) { }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checador_precios, container, false);
+        View view = inflater.inflate(R.layout.fragment_checador_precios, container, false);
+        initComponents(view);
+
+        return view;
+    }
+
+    private void initComponents(View view) {
+        fabFullScreen = view.findViewById(R.id.fcp_fullscreen);
+        fabFullScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
